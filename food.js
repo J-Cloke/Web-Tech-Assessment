860cf96e6e41840d48fb8bfe04d1c37d50556034
J-Cloke <123466448+J-Cloke@users.noreply.github.com>
2023-04-21 20:40:38 +0100

    moved to root folder to get GitHub Pages working

---
+++ food.js
@@ -0,0 +1,7 @@
+function toggle() {
+    var video = document.querySelector(".video")
+    video.classList.toggle("active");
+}
+
+var vid = document.getElementById("puerto-audio");
+vid.volume= 0.5;
