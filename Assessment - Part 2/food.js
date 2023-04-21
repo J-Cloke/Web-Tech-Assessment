fe84da6deeebec5a0d8a2b9878f3c996522bd709
J-Cloke <123466448+J-Cloke@users.noreply.github.com>
2023-04-21 21:30:48 +0100

    removed part 2 folder for zipping

--- Assessment - Part 2/food.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-    var video = document.querySelector(".video")
-    video.classList.toggle("active");
-}
-
-var vid = document.getElementById("puerto-audio");
-vid.volume= 0.5;
